aa357b537ae1f4158e1a19dadd399dc4eb306ace
Khadija2504 <khadijalady25@gmail.com>
2025-02-06 16:00:01 +0100

    REC-16 add the update component and style the colletcion list with the collection request form

--- src/app/collection/collection-routing.module.ts
+++ src/app/collection/collection-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CollectionRequestComponent } from './collection-request/collection-request.component';
 import { CollectionsListComponent } from './collections-list/collections-list.component';
+import { EditCollectionComponent } from './edit-collection/edit-collection.component';
 
 const routes: Routes = [
   {path: 'collection-request', component: CollectionRequestComponent},
-  {path: 'collections-list', component: CollectionsListComponent}
+  {path: 'collections-list', component: CollectionsListComponent},
+  {path: 'edit-collection/:id', component: EditCollectionComponent}
 ];
 
 @NgModule({
--- src/app/collection/collection.module.ts
+++ src/app/collection/collection.module.ts
@@ -5,12 +5,14 @@ import { CollectionRoutingModule } from './collection-routing.module';
 import { CollectionRequestComponent } from './collection-request/collection-request.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CollectionsListComponent } from './collections-list/collections-list.component';
+import { EditCollectionComponent } from './edit-collection/edit-collection.component';
 
 
 @NgModule({
   declarations: [
     CollectionRequestComponent,
-    CollectionsListComponent
+    CollectionsListComponent,
+    EditCollectionComponent
   ],
   imports: [
     CommonModule,
